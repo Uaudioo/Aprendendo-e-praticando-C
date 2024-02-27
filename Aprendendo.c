98b0bd004438f2087d7d94172cbb5a4a3ac91624
Wendell Jesus <159466021+Uaudioo@users.noreply.github.com>
2024-02-27 16:10:00 -0300

    Add files via upload

---
+++ Aprendendo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	//Declarando Va
+	//"char"equivale a cadeia 
+	char sexo = 'M';
+	char nome[200] = "Neymar";
+	//"int" Equivale a inteiro
+	int idade = 32;
+	//"Float" Equivale a Real
+	float peso = 68.00;	
+		
+		
+	
+	//Exibindo Dados
+	printf("\n=== Exibindo Dados=== \n");
+	printf("Nome: %s \n",nome);
+	printf("Sexo: %c \n",sexo);
+	printf("Idade: %i \n",idade);
+	printf("Peso: %f\n",peso);
+	
+	
+	
+	
+	
+	return 0;
+	
+}
